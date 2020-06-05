9d79ea6d651ea06e45e3ed5ad20531149f3b1d81
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-06-05 13:10:52 +0200

    Temporary login and auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import useSWR, { SWRConfig } from "swr";
 import { Container, Box, Typography } from "@material-ui/core";
-import fetcher from "./utils/fetch";
+import fetcher, { instance } from "./utils/fetch";
 
 function Pages() {
   const { data, error } = useSWR("/api/page/");
@@ -13,6 +13,30 @@ function Pages() {
   return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
 
+const UserAPI = () => {
+  const [token, setToken] = useState("");
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await instance.post("/auth/login/", { email: "root@example.org", password: "root" });
+      setToken(resp.data.key);
+    };
+    fetchData();
+  }, []);
+
+  console.log(token);
+
+  if (!token) return <div>Loading...</div>;
+  else {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { data } = useSWR(["/auth/user/", { headers: { Authorization: `Token ${token}` } }]);
+    setUser(data);
+  }
+
+  return <div>{JSON.stringify(user, null, 2)}</div>;
+};
+
 function App() {
   return (
     <React.StrictMode>
@@ -23,6 +47,7 @@ function App() {
               Hello, world!
             </Typography>
             <Pages />
+            <UserAPI />
           </Box>
         </Container>
       </SWRConfig>

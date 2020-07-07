c8befe567ea2f959edd3c0da907fad569ae0f1ed
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-07-07 17:21:59 +0200

    Remove SWR, add grapql-codegen and Apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,25 @@
-import React, { useState, useEffect } from "react";
-import useSWR, { SWRConfig } from "swr";
-import { Container, Box, Typography } from "@material-ui/core";
-import fetcher, { instance } from "./utils/fetch";
+import { Box, Container, Typography } from "@material-ui/core";
+import React from "react";
 
-function Pages() {
-  const { data, error } = useSWR("/api/page/");
-  console.log(data, error);
-
-  if (error) return <div>Error!!!!0101</div>;
-  if (!data) return <div>Loading...</div>;
-
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
-}
-
-const UserAPI = () => {
-  const [token, setToken] = useState("");
-  const [user, setUser] = useState({});
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const resp = await instance.post("/auth/login/", { email: "root@example.org", password: "root" });
-      setToken(resp.data.key);
-    };
-    fetchData();
-  }, []);
-
-  console.log(token);
-
-  if (!token) return <div>Loading...</div>;
-  else {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const { data } = useSWR(["/auth/user/", { headers: { Authorization: `Token ${token}` } }]);
-    setUser(data);
-  }
-
-  return <div>{JSON.stringify(user, null, 2)}</div>;
-};
+// const me = gql`
+//   query Me {
+//     me {
+//       id
+//       name
+//     }
+//   }
+// `;
 
 function App() {
   return (
     <React.StrictMode>
-      <SWRConfig value={{ fetcher: fetcher }}>
-        <Container maxWidth="sm">
-          <Box my={4}>
-            <Typography variant="h4" component="h1" gutterBottom>
-              Hello, world!
-            </Typography>
-            <Pages />
-            <UserAPI />
-          </Box>
-        </Container>
-      </SWRConfig>
+      <Container maxWidth="sm">
+        <Box my={4}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Hello, world!
+          </Typography>
+        </Box>
+      </Container>
     </React.StrictMode>
   );
 }

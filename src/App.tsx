8d960b41a455dd9bb9c8e1808f45f6b79a5da81f
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-07-07 19:24:30 +0200

    Add and configure Apollo, run test login mutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,79 @@
 import { Box, Container, Typography } from "@material-ui/core";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { setContext } from "apollo-link-context";
+import { createHttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
 import React from "react";
+import { ApolloProvider } from "react-apollo";
+import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
+import { useLoginMutationMutation } from "./generated/graphql";
 
-// const me = gql`
-//   query Me {
-//     me {
-//       id
-//       name
-//     }
-//   }
-// `;
+export const login = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      accessToken
+      refreshToken
+    }
+  }
+`;
+
+const link = createHttpLink({
+  uri: "http://localhost:3000/graphql",
+});
+
+const authLink = setContext((_, { headers }: { headers: object }) => {
+  const token = localStorage.getItem("accessToken");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(link),
+});
+
+const LogIn: React.FC = () => {
+  const [loginMutation, { data, error, loading }] = useLoginMutationMutation({
+    variables: {
+      email: "test@example.org",
+      password: "password123",
+    },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
+  console.log(JSON.stringify(data, null, 2));
+  console.log(JSON.stringify(loginMutation, null, 2));
+
+  return (
+    <div>
+      <p>Hello!</p>
+      <button onClick={() => loginMutation()}>Login</button>
+    </div>
+  );
+};
 
 function App() {
   return (
     <React.StrictMode>
-      <Container maxWidth="sm">
-        <Box my={4}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Hello, world!
-          </Typography>
-        </Box>
-      </Container>
+      <ApolloProvider client={client}>
+        <ApolloHooksProvider client={client}>
+          <Container maxWidth="sm">
+            <Box my={4}>
+              <Typography variant="h4" component="h1" gutterBottom>
+                Hello, world!
+              </Typography>
+              <LogIn />
+            </Box>
+          </Container>
+        </ApolloHooksProvider>
+      </ApolloProvider>
     </React.StrictMode>
   );
 }
---
+++ src/generated/graphql.tsx
@@ -0,0 +1,170 @@
+import gql from "graphql-tag";
+import * as ApolloReactCommon from "@apollo/react-common";
+import * as React from "react";
+import * as ApolloReactComponents from "@apollo/react-components";
+import * as ApolloReactHoc from "@apollo/react-hoc";
+import * as ApolloReactHooks from "@apollo/react-hooks";
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: any }> = { [K in keyof T]: T[K] };
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type User = {
+  __typename?: "User";
+  id: Scalars["Int"];
+  email: Scalars["String"];
+  password: Scalars["String"];
+  name: Scalars["String"];
+  avatar?: Maybe<Scalars["String"]>;
+  staff: Scalars["Boolean"];
+  admin: Scalars["Boolean"];
+  active: Scalars["Boolean"];
+};
+
+export type JwtToken = {
+  __typename?: "JwtToken";
+  /** JWT access token */
+  accessToken: Scalars["String"];
+  /** JWT refresh token */
+  refreshToken: Scalars["String"];
+};
+
+export type Query = {
+  __typename?: "Query";
+  me: User;
+  user: User;
+  users: Array<User>;
+};
+
+export type QueryUserArgs = {
+  id: Scalars["Int"];
+};
+
+export type Mutation = {
+  __typename?: "Mutation";
+  login: JwtToken;
+  changePassword: User;
+  refreshToken: JwtToken;
+  change: User;
+};
+
+export type MutationLoginArgs = {
+  email: Scalars["String"];
+  password: Scalars["String"];
+};
+
+export type MutationChangePasswordArgs = {
+  id: Scalars["Int"];
+  password: Scalars["String"];
+  newPassword: Scalars["String"];
+};
+
+export type MutationRefreshTokenArgs = {
+  token: Scalars["String"];
+};
+
+export type MutationChangeArgs = {
+  id: Scalars["Int"];
+  email?: Maybe<Scalars["String"]>;
+  name?: Maybe<Scalars["String"]>;
+  avatar?: Maybe<Scalars["String"]>;
+  staff?: Maybe<Scalars["Boolean"]>;
+  admin?: Maybe<Scalars["Boolean"]>;
+  active?: Maybe<Scalars["Boolean"]>;
+};
+
+export type LoginMutationMutationVariables = Exact<{
+  email: Scalars["String"];
+  password: Scalars["String"];
+}>;
+
+export type LoginMutationMutation = { __typename?: "Mutation" } & {
+  login: { __typename?: "JwtToken" } & Pick<JwtToken, "accessToken" | "refreshToken">;
+};
+
+export const LoginMutationDocument = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      accessToken
+      refreshToken
+    }
+  }
+`;
+export type LoginMutationMutationFn = ApolloReactCommon.MutationFunction<
+  LoginMutationMutation,
+  LoginMutationMutationVariables
+>;
+export type LoginMutationComponentProps = Omit<
+  ApolloReactComponents.MutationComponentOptions<LoginMutationMutation, LoginMutationMutationVariables>,
+  "mutation"
+>;
+
+export const LoginMutationComponent = (props: LoginMutationComponentProps) => (
+  <ApolloReactComponents.Mutation<LoginMutationMutation, LoginMutationMutationVariables>
+    mutation={LoginMutationDocument}
+    {...props}
+  />
+);
+
+export type LoginMutationProps<TChildProps = {}, TDataName extends string = "mutate"> = {
+  [key in TDataName]: ApolloReactCommon.MutationFunction<LoginMutationMutation, LoginMutationMutationVariables>;
+} &
+  TChildProps;
+export function withLoginMutation<TProps, TChildProps = {}, TDataName extends string = "mutate">(
+  operationOptions?: ApolloReactHoc.OperationOption<
+    TProps,
+    LoginMutationMutation,
+    LoginMutationMutationVariables,
+    LoginMutationProps<TChildProps, TDataName>
+  >,
+) {
+  return ApolloReactHoc.withMutation<
+    TProps,
+    LoginMutationMutation,
+    LoginMutationMutationVariables,
+    LoginMutationProps<TChildProps, TDataName>
+  >(LoginMutationDocument, {
+    alias: "loginMutation",
+    ...operationOptions,
+  });
+}
+
+/**
+ * __useLoginMutationMutation__
+ *
+ * To run a mutation, you first call `useLoginMutationMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useLoginMutationMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [loginMutationMutation, { data, loading, error }] = useLoginMutationMutation({
+ *   variables: {
+ *      email: // value for 'email'
+ *      password: // value for 'password'
+ *   },
+ * });
+ */
+export function useLoginMutationMutation(
+  baseOptions?: ApolloReactHooks.MutationHookOptions<LoginMutationMutation, LoginMutationMutationVariables>,
+) {
+  return ApolloReactHooks.useMutation<LoginMutationMutation, LoginMutationMutationVariables>(
+    LoginMutationDocument,
+    baseOptions,
+  );
+}
+export type LoginMutationMutationHookResult = ReturnType<typeof useLoginMutationMutation>;
+export type LoginMutationMutationResult = ApolloReactCommon.MutationResult<LoginMutationMutation>;
+export type LoginMutationMutationOptions = ApolloReactCommon.BaseMutationOptions<
+  LoginMutationMutation,
+  LoginMutationMutationVariables
+>;

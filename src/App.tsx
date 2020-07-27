2c88bedaac6166251bb761ac91a48ea9382ac9a1
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-07-27 22:52:12 +0200

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ApolloProvider } from "@apollo/client";
 import { Container, CssBaseline, makeStyles, Typography } from "@material-ui/core";
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { apolloClient } from "./Apollo";
 import { Copyright } from "./Copyright";
 import SignIn from "./Login";
@@ -22,28 +23,44 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const Main: React.FC = () => {
+  return (
+    <>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Hello, world!
+      </Typography>
+      <Typography variant="body1">Sticky footer placeholder.</Typography>
+    </>
+  );
+};
+
 function App() {
   const classes = useStyles();
 
   return (
     <React.StrictMode>
       <ApolloProvider client={apolloClient}>
-        <div className={classes.root}>
-          <CssBaseline />
-          <Container component="main" className={classes.main} maxWidth="sm">
-            <Typography variant="h4" component="h1" gutterBottom>
-              Hello, world!
-            </Typography>
-            <Typography variant="body1">Sticky footer placeholder.</Typography>
-            <SignIn />
-          </Container>
-          <footer className={classes.footer}>
-            <Container maxWidth="sm">
-              <Typography variant="body1">My sticky footer can be found here.</Typography>
-              <Copyright />
+        <Router>
+          <div className={classes.root}>
+            <CssBaseline />
+            <Container component="main" className={classes.main} maxWidth="sm">
+              <Switch>
+                <Route path="/login">
+                  <SignIn />
+                </Route>
+                <Route exact path="/">
+                  <Main />
+                </Route>
+              </Switch>
             </Container>
-          </footer>
-        </div>
+            <footer className={classes.footer}>
+              <Container maxWidth="sm">
+                <Typography variant="body1">My sticky footer can be found here.</Typography>
+                <Copyright />
+              </Container>
+            </footer>
+          </div>
+        </Router>
       </ApolloProvider>
     </React.StrictMode>
   );

7a41be25199b78e8415841f83dbd7e5e622cd9b1
Christian Hein <scheiehein@gmail.com>
2021-03-13 16:32:14 +0100

    [CH-114]: Submit button posts JSON to server

--- src/components/AddCompanyForm.tsx
+++ src/components/AddCompanyForm.tsx
@@ -1,15 +1,16 @@
 import { makeStyles } from "@material-ui/core/styles";
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import React from "react";
+import axios from "axios";
 
 type FormData = {
-  companyName: string;
-  companyURL: string;
+  name: string;
+  homepage: string;
 };
 
 const useStyles = makeStyles((theme) => ({
@@ -22,19 +23,37 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const postRequest = async (msg: any) => {
+  /*
+  const newTodo = {
+    userId: 1,
+    title: "Wash my hands",
+    completed: false,
+  };
+  */
+  console.log(msg);
+  try {
+    const resp = await axios.post("https://echo-karriere-dev.azurewebsites.net/api/companies", msg);
+    console.log(resp.data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// TODO: Add participation checkbox
 export default function AddCompanyForm() {
   const { register, handleSubmit } = useForm<FormData>();
-  const onSubmit = (data: any) => console.log(data);
+  const onSubmit: SubmitHandler<FormData> = (data) => postRequest(JSON.stringify(data));
   const classes = useStyles();
   const [state, setState] = React.useState({
-    confirmedParticipationCheckBox: false,
+    participation: false,
   });
 
   const handleChange = (event: any) => {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
 
-  const { confirmedParticipationCheckBox } = state;
+  const { participation } = state;
 
   return (
     <Container component="main" maxWidth="sm">
@@ -45,9 +64,9 @@ export default function AddCompanyForm() {
           margin="normal"
           required
           fullWidth
-          id="companyName"
+          id="name"
           label="Bedriftsnavn"
-          name="companyName"
+          name="name"
           autoFocus
           inputRef={register({ required: "Obligatorisk felt" })}
         />
@@ -56,20 +75,13 @@ export default function AddCompanyForm() {
           margin="normal"
           required
           fullWidth
-          name="companyURL"
+          name="homepage"
           label="Bedriftens hjemmeside"
-          id="companyURL"
+          id="homepage"
           inputRef={register({ required: "Obligatorisk felt" })}
         />
         <FormControlLabel
-          control={
-            <Checkbox
-              checked={confirmedParticipationCheckBox}
-              onChange={handleChange}
-              name="confirmedParticipationCheckBox"
-              id="confirmedParticipationCheckBox"
-            />
-          }
+          control={<Checkbox checked={participation} onChange={handleChange} name="participation" id="participation" />}
           label="Bekreftet deltagelse"
           name="confirmedParticipation"
           inputRef={register}

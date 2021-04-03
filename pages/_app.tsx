c4b70599edd4c1948c63b425549ea19494ae8def
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-04 00:22:43 +0200

    Initial pass at Apollo GraphQL integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,22 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../lib/apollo";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  const client = useApollo(pageProps);
+
   return (
     <>
       <Head>
         <title>echo karriere dashboard</title>
       </Head>
-      <ChakraProvider resetCSS>
-        <Component {...pageProps} />
-      </ChakraProvider>
+      <ApolloProvider client={client}>
+        <ChakraProvider resetCSS>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </ApolloProvider>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import { DarkModeSwitch, Container } from "../components";
+import { CompaniesList } from "../components/companies/CompaniesList";
+import { initializeApollo } from "../lib/apollo";
+import type { NormalizedCacheObject } from "@apollo/client";
 
 export default function Index(): JSX.Element {
   return (
     <Container>
       <DarkModeSwitch />
       <p>Hello, world!</p>
+      <CompaniesList />
     </Container>
   );
+}
+
+export function getStaticProps(): { props: { initialApolloState: NormalizedCacheObject } } {
+  const client = initializeApollo();
+
+  return {
+    props: {
+      initialApolloState: client.cache.extract(),
+    },
+  };
 }

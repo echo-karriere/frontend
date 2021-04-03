31945a54d5c80d7d0795fe5704e71ef8dc60e23f
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-03 21:43:04 +0200

    A very basic initial Chakra-UI layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import { Container } from "@chakra-ui/react";
+import { DarkModeSwitch, Container } from "../components";
 
 export default function Index(): JSX.Element {
   return (
     <Container>
+      <DarkModeSwitch />
       <p>Hello, world!</p>
     </Container>
   );

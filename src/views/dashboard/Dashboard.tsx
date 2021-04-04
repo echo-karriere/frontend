b859fb9cef7f29abbf56da836dbf070b133e5358
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-05 00:15:17 +0200

    Create landing page for logging in via Auth0

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -15,7 +15,7 @@ import Paper from "@material-ui/core/Paper";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import NotificationsIcon from "@material-ui/icons/Notifications";
-import { Copyright } from "../../Copyright";
+import { Copyright } from "../../components";
 import { Chart, Deposits, ListRouter, Orders } from "./components";
 
 const drawerWidth = 240;

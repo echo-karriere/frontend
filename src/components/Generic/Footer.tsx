011f98627586cd995f403fa60952bfdef4f7a246
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-05 23:12:51 +0200

    Remove styles.ts file, move remainder of code into Footer component

--- src/components/Generic/Footer.tsx
+++ src/components/Generic/Footer.tsx
@@ -1,6 +1,13 @@
-import { Container, Typography } from "@material-ui/core";
+import { Container, makeStyles, Typography } from "@material-ui/core";
 import { Copyright } from "../index";
-import { useStyles } from "../../styles";
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: "auto",
+    backgroundColor: theme.palette.type === "light" ? theme.palette.grey[200] : theme.palette.grey[800],
+  },
+}));
 
 export const Footer = (): JSX.Element => {
   const classes = useStyles();

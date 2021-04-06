d311993fa5874b280bb35215a078398378ed73b1
Marie Heggebakk <marie.heggebakk@gmail.com>
2021-04-06 21:33:44 +0200

    [CH-111]: GraphQL queries, add button to add new company and refresh companies

--- src/components/ShowCompanies.tsx
+++ src/components/ShowCompanies.tsx
@@ -1,6 +1,10 @@
-import { Container, createStyles, Theme, Typography } from "@material-ui/core";
+import { Box, Container, createStyles, IconButton, Theme, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { DataGrid, GridColDef } from "@material-ui/data-grid";
+import { useAllCompaniesQuery } from "../graphql";
+import { Spinner } from "./Generic";
+import { Link } from "react-router-dom";
+import { AddCircleOutlineOutlined, Refresh } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -13,31 +17,44 @@ const useStyles = makeStyles((theme: Theme) =>
     table: {
       maxWidth: "sm",
       maxHeight: "sm",
+      marginTop: theme.spacing(2),
     },
   }),
 );
 
-const companies = [
-  { id: 1, companyName: "VG", website: "vg.no" },
-  { id: 2, companyName: "NRK", website: "nrk.no" },
-];
-
 const columns: GridColDef[] = [
-  { field: "companyName", headerName: "Company name", width: 170 },
-  { field: "website", headerName: "Website", width: 200 },
+  { field: "name", headerName: "Company name", width: 230 },
+  { field: "homepage", headerName: "Website", width: 200 },
 ];
 
 export const ShowCompanies = (): JSX.Element => {
   const classes = useStyles();
-
+  const { data, loading, error, refetch } = useAllCompaniesQuery();
+  if (loading) {
+    return <Spinner />;
+  }
+  if (error || !data) {
+    console.error(error);
+    return <p>Error!</p>;
+  }
   return (
-    <Container maxWidth={"sm"} component="main" className={classes.root}>
+    <Container maxWidth={"sm"} component="main" className={classes.root} aria-label={"Table of companies"}>
       <Typography component="h2" variant="h6" color="primary" gutterBottom>
         Companies
       </Typography>
       <div className={classes.table}>
+        <Box display={"flex"} justifyContent={"flex-end"}>
+          <label htmlFor={"refresh-button"}>
+            <IconButton color={"primary"} aria-label={"refresh companies"} component={"span"} onClick={() => refetch()}>
+              <Refresh />
+            </IconButton>
+          </label>
+          <IconButton color={"primary"} aria-label={"add new company"} component={Link} to={"/companies/create"}>
+            <AddCircleOutlineOutlined />
+          </IconButton>
+        </Box>
         <DataGrid
-          rows={companies}
+          rows={data.allCompanies}
           columns={columns}
           pageSize={10}
           rowsPerPageOptions={[10, 15, 25]}
--- src/views/companies/CompanyOverview.tsx
+++ src/views/companies/CompanyOverview.tsx
@@ -1,13 +1,8 @@
-import { DashboardWrapper } from "../../components";
-import { Link } from "react-router-dom";
-import { ShowCompanies } from "../../components/ShowCompanies";
+import { DashboardWrapper, ShowCompanies } from "../../components";
 
 export const CompanyOverview = (): JSX.Element => {
   return (
     <DashboardWrapper title="Companies">
-      <Link to="/companies/create">
-        <p>Create new company</p>
-      </Link>
       <ShowCompanies />
     </DashboardWrapper>
   );

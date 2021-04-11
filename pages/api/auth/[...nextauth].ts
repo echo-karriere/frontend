9fed0656775be593045ab81487d9444e977ad4d1
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-11 18:47:52 +0200

    [CH-126]: Get accessToken and roles, add to session/JWT token

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,7 @@
-import NextAuth from "next-auth";
+/* eslint-disable @typescript-eslint/require-await */
+import NextAuth, { Session } from "next-auth";
+import { JWT } from "next-auth/jwt";
+import { WithAdditionalParams } from "next-auth/_utils";
 
 export default NextAuth({
   providers: [
@@ -18,7 +21,9 @@ export default NextAuth({
       requestTokenUrl: `${process.env.KEYCLOAK_BASE_URL}/auth`,
       authorizationUrl: `${process.env.KEYCLOAK_BASE_URL}/auth?response_type=code`,
       profileUrl: `${process.env.KEYCLOAK_BASE_URL}/userinfo`,
-      profile: (profile: Record<string, string>) => ({ ...profile, id: profile.sub }),
+      profile: (profile: Record<string, string>) => {
+        return { ...profile, id: profile.sub };
+      },
     },
   ],
   secret: process.env.SECRET,
@@ -27,5 +32,25 @@ export default NextAuth({
   },
   pages: {
     signIn: "/",
+  },
+  callbacks: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async jwt(token, user, account, _profile, _isNewUser): Promise<WithAdditionalParams<JWT>> {
+      const usr = user as { resource_access: { backend: { roles: Array<string> } } };
+      if (account?.accessToken) {
+        token.accessToken = account.accessToken;
+        token.roles = usr.resource_access.backend.roles;
+      }
+
+      return token;
+    },
+    async session(session, token): Promise<WithAdditionalParams<Session>> {
+      const tok = token as JWT;
+      const sess = (session as unknown) as Record<string, unknown>;
+      sess.accessToken = tok?.accessToken as string;
+      sess.roles = tok?.roles as Array<string>;
+
+      return sess as WithAdditionalParams<Session>;
+    },
   },
 });
